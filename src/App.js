886f2f37ddf25cf8c768e72ba841ac7fcf142be6
Wellison M. Dias <wellisonmdias@gmail.com>
2023-04-21 00:20:14 -0300

    resolve reqs16 e 18

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Config from './pages/Configs';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
@@ -34,6 +35,11 @@ export default function App() {
           exact
           path="/feedbacks"
           component={ Feedback }
+        />
+        <Route
+          exact
+          path="/ranking"
+          component={ Ranking }
         />
       </Switch>
     </div>
