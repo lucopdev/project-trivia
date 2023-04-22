9c138f2b547f553392683b0379df657b529f41b4
lucopdev <lucopdev@gmail.com>
2023-04-22 08:09:06 -0300

    group 5 - requisito 17fix

--- src/tests/game.test.js
+++ src/tests/game.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
-// import userEvent from '@testing-library/user-event';
-import { screen } from '@testing-library/react';
-// import { act } from 'react-dom/test-utils';
+import { screen, waitFor } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
-// import Login from './pages/Login';
 import { initialState, token } from './helpers/initialStateMock';
-// import App from './App';
 import Game from '../pages/Game';
 import { rankingStorageMock } from './helpers/rankingStorageMock';
 import { questions } from './helpers/questionsMock';
+// import userEvent from '@testing-library/user-event';
+// import Login from './pages/Login';
+// import App from './App';
 
 describe('Testa a pagina de game', () => {
   beforeEach(() => {
@@ -25,18 +25,28 @@ describe('Testa a pagina de game', () => {
     mockQuestions();
   });
   it('Verifica se os elementos são renderizado na tela', async () => {
-    renderWithRouterAndRedux(<Game />, { initialState });
-
-    expect(await screen.findByTestId('timer')).toBeInTheDocument();
-    expect(await screen.findByTestId('question-text')).toBeInTheDocument();
-    expect(await screen.findByTestId('question-category')).toBeInTheDocument();
-    expect(await screen.findByTestId('answer-options')).toBeInTheDocument();
-    const correctBtn = await screen.findByTestId('correct-answer');
-
-    expect(correctBtn).toBeInTheDocument();
-
-    if(questions.response_code === 3) {
+    const { history } = renderWithRouterAndRedux(<Game />, { initialState });
+    const responseCode = 3;
+    const timeOut = 1000;
+    if (questions.response_code === responseCode) {
       expect(!JSON.parse(localStorage.getItem('token')));
+      act(() => {
+        history.push('/');
+      });
+      history.location.pathname('/');
+    } else {
+      const timer = await screen.findByTestId('timer');
+      const correctBtn = await screen.findByTestId('correct-answer');
+
+      expect(timer).toBeInTheDocument();
+      expect(timer.innerHTML).toBe('30');
+      await waitFor(() => {
+        expect(timer.innerHTML).toBe('29');
+      }, timeOut);
+      expect(await screen.findByTestId('question-text')).toBeInTheDocument();
+      expect(await screen.findByTestId('question-category')).toBeInTheDocument();
+      expect(await screen.findByTestId('answer-options')).toBeInTheDocument();
+      expect(correctBtn).toBeInTheDocument();
     }
     // questions.results.forEach( async (questionA, outerIndex) => {
     //   const correctBtn = await screen.findByTestId('correct-answer');
